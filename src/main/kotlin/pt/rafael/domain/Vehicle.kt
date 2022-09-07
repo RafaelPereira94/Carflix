af9d5919e92962f9e320882dfbaee97edef08687
Rafael <rafaelcarmopereira@gmail.com>
2022-09-07 22:15:02 +0100

    Add Vehicle Domain type.

---
+++ src/main/kotlin/pt/rafael/domain/Vehicle.kt
@@ -0,0 +1,31 @@
+package pt.rafael.domain
+
+import kotlinx.datetime.LocalDateTime
+import org.bson.codecs.pojo.annotations.BsonId
+import org.litote.kmongo.Id
+
+data class Vehicle(
+    @BsonId
+    val id: Id<Vehicle>? = null,
+    val brand: String,
+    val model: String,
+    val version: String,
+    val images: List<String>,
+    val price: Double,
+    val doors: Int,
+    val year: Int,
+    val month: Int,
+    val kilometer: Int,
+    val miles: Double,
+    val fuel: String, //TODO enum
+    val segment: List<String>, //TODO enum,
+    val transmission: String,
+    val usage: String,
+    val color: String,
+    val description: String,
+    val isDeleted: Boolean,
+    val isReserved: Boolean,
+    val isSold: Boolean,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime
+)
