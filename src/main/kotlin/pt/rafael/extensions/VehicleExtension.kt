7fe4ed00ea11b9383286cce5fec46f6bb94b0e2d
Rafael <rafaelcarmopereira@gmail.com>
2022-11-03 12:07:31 +0000

    Fix extension functions of vehicle.

--- src/main/kotlin/pt/rafael/extensions/VehicleExtension.kt
+++ src/main/kotlin/pt/rafael/extensions/VehicleExtension.kt
@@ -3,56 +3,53 @@ package pt.rafael.extensions
 import pt.rafael.domain.Vehicle
 import pt.rafael.domain.dto.VehicleDto
 
-class VehicleExtension {
+fun Vehicle.toDto(): VehicleDto =
+    VehicleDto(
+        id = this.id.toString(),
+        brand,
+        model,
+        version,
+        images,
+        price,
+        doors,
+        year,
+        month,
+        kilometer,
+        miles,
+        fuel,
+        segment,
+        transmission,
+        usage,
+        color,
+        description,
+        isDeleted,
+        isReserved,
+        isSold,
+        createdAt,
+        updatedAt
+    )
 
-    fun Vehicle.toDto(): VehicleDto =
-        VehicleDto(
-            id = this.id.toString(),
-            brand,
-            model ,
-            version,
-            images,
-            price,
-            doors,
-            year,
-            month,
-            kilometer,
-            miles,
-            fuel,
-            segment,
-            transmission,
-            usage,
-            color,
-            description,
-            isDeleted,
-            isReserved,
-            isSold,
-            createdAt,
-            updatedAt
-        )
-
-    fun VehicleDto.toVehicle(): Vehicle =
-        Vehicle(
-            brand = this.brand,
-            model = this.model,
-            version = this.version,
-            images = this.images,
-            price = this.price,
-            doors = this.doors,
-            year = this.year,
-            month = this.month,
-            kilometer = this.kilometer,
-            miles = this.miles,
-            fuel = this.fuel,
-            segment = this.segment,
-            transmission = this.transmission,
-            usage = this.usage,
-            color = this.color,
-            description = this.description,
-            isDeleted = this.isDeleted,
-            isReserved = this.isReserved,
-            isSold = this.isSold,
-            createdAt = this.createdAt,
-            updatedAt = this.updatedAt
-        )
-}
+fun VehicleDto.toVehicle(): Vehicle =
+    Vehicle(
+        brand = this.brand,
+        model = this.model,
+        version = this.version,
+        images = this.images,
+        price = this.price,
+        doors = this.doors,
+        year = this.year,
+        month = this.month,
+        kilometer = this.kilometer,
+        miles = this.miles,
+        fuel = this.fuel,
+        segment = this.segment,
+        transmission = this.transmission,
+        usage = this.usage,
+        color = this.color,
+        description = this.description,
+        isDeleted = this.isDeleted,
+        isReserved = this.isReserved,
+        isSold = this.isSold,
+        createdAt = this.createdAt,
+        updatedAt = this.updatedAt
+    )
